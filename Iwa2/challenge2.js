d7af0e57845a8cfe0015692821e4a8c4ea6d82c9
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-02 09:10:04 +0200

    challenge1-3

---
+++ Iwa2/challenge2.js
@@ -0,0 +1,8 @@
+/**(c) ACME Inc 2010 */
+
+/**
+ * This is the data that a user created their content
+ */
+const date = '10/07/2014'
+
+console.log(date);
---
+++ Iwa2/challenge3.js
@@ -0,0 +1,12 @@
+/*
+It is important to show the following meassage in order to:
+-to assure users
+-to scare hackers 
+- to impress investors 
+ */
+
+console.warn('Security scan starting')
+
+//It is important to let user know when they van close the page //
+
+console.info('please wait for the scan to complete before closing broswser')
