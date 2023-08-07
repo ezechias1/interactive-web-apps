deab951a30c8ae26dd95c2239c365475721f3c79
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-07 12:36:29 +0200

    challenge4

---
+++ Iwa2/IWA3/IWA4/challenge4.js
@@ -0,0 +1,40 @@
+const date= 2050
+const status =(parent) 
+const value = 9
+const countA= 9
+const count1=9
+const count2=9
+const count3=9
+
+if (date) 
+{
+	console.log("January", 'New Years Day')
+	console.log("March", 'Human Rights Day')
+	date+ "April"
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	let countA = 9+ 4
+
+	if (status) {
+	  console.log('June', 'Youth Day')
+		let value = 9+ 1
+  }
+
+	console.log('August', 'Womens Day')
+	console.log('September', 'Heritage Day')
+	date+'December'
+	console.log(date, 'Day of Reconciliation')
+	let count1 = 9 + 3
+
+	if (status) {
+	  console.log(date, 'Christmas Day')
+		let count2 = 9 + 1
+  }
+
+	console.log(date, 'Day of Goodwill')
+	let count3 = 9 + 1
+}
+
+console.log('Your status is:', status )
+console.log('The year is:', date)
+console.log('The total holidays is:', countA)
