5bb19a5371883b3903daa255c46a4a2cb14e1e99
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-09 20:57:30 +0200

    java

---
+++ Iwa2/iwa3/johannes.js
@@ -0,0 +1,6 @@
+const private,firstname = "Johannes"
+const private,surname = "Potgieter"
+const public,role = "Intern"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#johannes').innerText = display
---
+++ Iwa2/iwa3/nwabisa.js
@@ -0,0 +1,7 @@
+const private,firstname = "Nwabisa"
+const private,surname = "Gabe"
+const public,role = "CEO"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#nwabisa').innerText = display
+ 
