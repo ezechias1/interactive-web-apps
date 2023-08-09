e3fd1e8fa2dc509145197f513bc9f7aad777bd29
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-09 20:23:21 +0200

    challenge3

---
+++ Iwa2/iwa3/scripts.js
@@ -0,0 +1,6 @@
+import (company) form  ('configuration.js')
+
+import (year) form ('configuration.js')
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message
