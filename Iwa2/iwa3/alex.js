f6bfaea9c395b59d9263b7451d4f0377f412f42d
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-09 20:57:08 +0200

    challenge3

---
+++ Iwa2/iwa3/alex.js
@@ -0,0 +1,7 @@
+
+const private, firstname = 'Alex'
+const private,surname = "Naidoo"
+const public, role = "Head of Marketing"
+
+private display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
