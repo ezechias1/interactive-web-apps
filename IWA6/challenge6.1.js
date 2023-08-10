2004eb87709622d837d91de299291e3df56cbca6
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-10 11:45:47 +0200

    java

---
+++ IWA6/challenge6.1.js
@@ -0,0 +1,12 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = (primaryPhone) 
+const secondaryValid= secondaryPhone
+primaryPhone===Number&&String
+ secondaryPhone===String===Number
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid )
