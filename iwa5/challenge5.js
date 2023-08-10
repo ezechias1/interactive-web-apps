a652da0b243a360859c2f1b189428774d059bcb0
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-10 11:45:40 +0200

    java

--- iwa5/challenge5.js
+++ iwa5/challenge5.js
@@ -2,34 +2,32 @@ const FREE_WARNING = 'Free shipping only applies to single customer orders'
 const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
 const NONE_SELECTED = '0'
 
-if (location === RSA)  { shipping === 400 && currency === 'R' }
+ const customers=1
+ let location= 'RSA'
+ let currency= '$'
+ let shipping=800,
 
- location === NAM
-shipping = 600 
-else:shipping = 800
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
-
+const shoes = 300 * 1
+const toys = 100 * 5
+const shirts = 150 * 'NONE_SELECTED'
+const batteries =35 * 2
+const pens = 5 * 'NONE_SELECTED' 
+const total= shoes+toys+ shirts+batteries+pens
 shipping = null
 currency = $
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+if (location=== "RSA") {shipping=400 currency && =='R'}
+if (location=== "NAM") {}
+	else{shipping=600}
 
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+if (total>=1000 && currency 'R' || total>600 && currency) 
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+if (shipping===0 && currency!==1) {console.log(FREE_WARNING)}
 
-customers = '1'
-const location = 'RSA'
-currency = null
+if (loacation==='NK') {console.log(BANNED_WARNIN) "WARNING":}
+else{console.log ('price', currency,total)}
+	
+
  
