d37973c0cec8fa64da011d4973a422761f82742a
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-13 21:55:54 +0200

    added iwa7

--- iwa7/challenge7.2.js
+++ iwa7/challenge7.2.js
@@ -4,10 +4,10 @@ const firstname = "Timothy";
 if(first){
 const message = `Good Morning,  ${first}!`}
 else if(second){            
-    const= `Good Morning, ${second}!`
+    const= `Good Morning,  ${second}!`
 }
 else{
     const message=`Good Morning`
 }
 
-console.log("Good Morning, ${nickname} || {firstname}!")
+console.log(`Good Morning, ${nickname} || {firstname}!`)
