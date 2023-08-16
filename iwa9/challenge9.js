3a42b7456a092f627f8e9144d0250a2460adc7f8
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-16 17:01:08 +0200

    so hard

--- iwa9/challenge9.js
+++ iwa9/challenge9.js
@@ -31,7 +31,7 @@ const rent = {
 // You can change below however you want
 
 const taxAsDecimal = tax['913']/ 100 (parseFloat)  
-const startingAfterTax =salary -1 - taxAsDecimal
+const startingAfterTax = 1 - taxAsDecimal
 const type = size+lodging
 const balance = expenses('transport') - expenses('food') - expenses('rent.[type]')
 console.log(balance)
