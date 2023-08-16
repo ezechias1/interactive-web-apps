d3d92d6518368f7fba229cad04bba6c30814eb33
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-16 16:26:17 +0200

    iwa9

--- iwa9/challenge9.js
+++ iwa9/challenge9.js
@@ -6,7 +6,7 @@ const size = 'large'
 // Only change the syntax below (not the values or key names)
 
 const expenses = {
-    food :  51.7501,
+    food : 51.7501,
     transport:10.2,
 }
   
@@ -30,10 +30,11 @@ const rent = {
 
 // You can change below however you want
 
-const taxAsDecimal = tax/ 100
-const startingAfterTax =( salary * 1 - taxAsDecimal)
-const type =( lodging + size)
-const balance =(expenses(transport) - expenses(food) - expenses(rent.type)) 
+const taxAsDecimal = tax['913']/ 100 
+ parseFloat (taxAsDecimal) 
+const startingAfterTax =salary * 1 - taxAsDecimal
+const type = lodging + size
+const balance =expenses(transport) - expenses(food) - expenses(rent[type])
 console.log(balance)
  
 
