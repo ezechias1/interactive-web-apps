8879f3b6bb2749bb423656e20a73e4bc93af1d51
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-24 10:27:48 +0200

    iwa14

---
+++ iwa14/challenge14.1.js
@@ -0,0 +1,14 @@
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = () => { hobby                       
+  console(parameter)
+  console(parameter)
+}
+
+function hobby () {
+  logTwice(`Hello, ${firstName} ${age}. I love ${hobby}!`)
+}
+
+console.log(hobby())
