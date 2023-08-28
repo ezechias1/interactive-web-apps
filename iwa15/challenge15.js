7f4e7a1a1ea83d6d56d0263825b56189e22a676a
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-08-28 11:29:35 +0200

    iwa15

---
+++ iwa15/challenge15.js
@@ -0,0 +1,55 @@
+// scripts.js
+
+const data = {
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
+}
+
+// Only edit below
+
+const { first = 1 } = data.first || {}
+const { second = 1 } = data.second || {}
+const { third = 1 } = data.third || {}
+
+const result = [ ]
+console.log(first)
+console.log(second)
+console.log(third)
+
+const extractBiggest = () => {
+    if (first[first.length -1] > second[second.length -1]) {
+        return first
+    }
+
+    if (third[third.length -1] < 1) {
+        return second
+    }
+    
+    return third
+}
+
+
+// Only edit above
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+console.log(result)
