fb1afe52a0f23f0bba524d2018444750a6cac9b0
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2023-10-17 10:57:39 +0200

    iwa1

---
+++ iwa/iwa1/challenge.js
@@ -0,0 +1,19 @@
+const number = document.querySelector('[data-key="number"]')
+const subtract= document.querySelector('[data-key="subtract"]')
+const add = document.querySelector('[data-key="add"]')
+
+const subtractHandler =() =>{
+    const newValue = parseInt(number.value) -1
+    number.value = newValue ;
+    console.log ('subtract');
+}
+
+const addHandler =() =>{
+    const newValue = parseInt(number.value) +1
+    number.value = newValue ;
+    console.log ('add');
+}
+
+subtract.addEventListener('click', subtractHandler)
+
+add.addEventListener('click', addHandler)
